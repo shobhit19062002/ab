605e68b673e23e7349633b5af868f97a8cec863d
shobhit19062002 <shobhitchauhan2002@gmail.com>
2024-07-04 22:46:07 +0530

    change multiplication

--- mul.c
+++ mul.c
@@ -1,7 +1,10 @@
-#include<stdio.h> 
+#include <stdio.h>
 
 int main()
 {
-    
+    printf("Enter two number to get multiplication of the number: ");
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a * b);
     return 0;
-}	
+}
